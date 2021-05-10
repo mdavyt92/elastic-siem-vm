71afac4a7788b1ac00b5c90725a406c8bedecb5f
elisanabria <69690691+elisanabria@users.noreply.github.com>
2021-05-10 16:49:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,9 +29,9 @@ install_docker_centos() {
   yum -y update
 
   echo "Installing docker..."
-  yum install -y yum-utils device-mapper-persistent-data lvm2
+  yum -y install yum-utils device-mapper-persistent-data lvm2
   yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-  yum install docker-ce
+  yum -y install docker-ce
   curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
   systemctl restart docker
